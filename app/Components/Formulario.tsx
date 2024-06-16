81f9b06f37ac18976c601d5c9ad10954a91716bd
padilla2606 <naiyobis86@gmail.com>
2024-06-15 21:53:38 -0300

    Web creada con next js

---
+++ app/Components/Formulario.tsx
@@ -0,0 +1,15 @@
+import styles from "./Formulario.module.css"
+export const Formulario = ()=>{
+    return(
+        <div className={styles.Formulario}>
+            <h1 className={styles.h1}>Comenzamos?</h1>
+            <p className={styles.psubtitle}>Envianos un mensaje atravez del formulario y te atenderemos enseguida</p>
+            <div className={styles.boxinput}>
+                <div className={styles.cajitainput}><p>Nombre:</p><input className={styles.input} type="text" placeholder="Nombre"/></div>
+                <div className={styles.cajitainput}><p>Email:</p><input className={styles.input} type="email" placeholder="Correo@gmail.com"/></div>
+                <div className={styles.cajitainput}><p>Mensaje:</p><input className={styles.inputarea} type="texbox" placeholder="Mensaje"/></div>
+                <button className={styles.enviar}>Enviar</button>
+            </div>
+        </div>
+    )
+}
